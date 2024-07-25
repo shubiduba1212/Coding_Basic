02143f6db149cb17f25cefa673dd6df63427e772
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-07-26 01:19:39 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 1.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        char[] arr = overwrite_string.toCharArray();
+        char[] arr2 = my_string.toCharArray();
+        String toBeReplaced = my_string.substring(s, (arr.length + s));
+        System.out.println(my_string.indexOf(toBeReplaced));
+        System.out.println(my_string.lastIndexOf(toBeReplaced));
+        if(my_string.indexOf(toBeReplaced) != s){
+            System.out.println(my_string.substring(0, s));
+            if(arr2.length == (s + 1)){
+                answer = my_string.substring(0, s).concat(overwrite_string);
+            }else{
+                System.out.println(my_string.substring(arr.length + s));
+                answer = my_string.substring(0, s).concat(overwrite_string).concat(my_string.substring(arr.length + s));
+            }
+        }else{
+            answer = my_string.replaceFirst(toBeReplaced, overwrite_string);
+        }
+        return answer;
+    }
+}
