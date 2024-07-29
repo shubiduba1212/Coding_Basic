167d027a5fd8c9f4e50781a51c05399b2a76e632
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-07-29 23:22:12 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+    for (int i = 0; i < num_list.length - 1; i++) {
+      for(int j = 0; j < num_list.length - i - 1; j++){
+          if(num_list[j] > num_list[j+1]){
+              int num = num_list[j];
+              num_list[j] = num_list[j+1];
+              num_list[j+1] = num;
+          }          
+      }       
+    }
+        
+    answer = new int[5];
+        
+    for(int i = 0; i < 5; i++){
+        answer[i] = num_list[i];
+    }
+        
+        return answer;
+    }
+}
