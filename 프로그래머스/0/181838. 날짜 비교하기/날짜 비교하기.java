9d9ab0d3cd8bdc34c9c8842c70edb96d6e84d60b
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-09-01 00:00:25 +0900

    Update 날짜 비교하기.java

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -3,11 +3,11 @@ class Solution {
         int answer = 0;
         for(int i = 0; i < 3; i++){            
             if(date1[i] < date2[i]){
-                return 1; // date1이 date2보다 앞선다.
+                return 1; 
             } else if(date1[i] > date2[i]){
-                return 0; // date1이 date2보다 늦다.
+                return 0; 
             }
         }
-        return 0; // 두 날짜가 같은 경우
+        return 0; 
     }
 }
