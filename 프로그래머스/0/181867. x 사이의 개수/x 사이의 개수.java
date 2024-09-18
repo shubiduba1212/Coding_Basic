cf3f771fe50e25718c0f1837a5183746dbd37950
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-09-18 23:26:04 +0900

    Update x 사이의 개수.java

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -7,7 +7,6 @@ class Solution {
         for (String s : myArray) {
           answer[index] = s.length();
           index++;
-            System.out.println(s);
         }
         return answer;
     }
