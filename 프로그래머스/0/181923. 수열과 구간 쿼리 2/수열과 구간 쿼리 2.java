ccc549f1afe96d028312e48cfb6c22cbcf84158d
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-11-05 23:25:29 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.78 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        answer = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            int start = queries[i][0];
+            int end = queries[i][1];
+            int compare = queries[i][2];
+            int minNum = 1000000;
+            boolean check = false;
+            
+            for(int j = start; j <= end; j++){
+                if(arr[j] > compare && arr[j] < minNum){
+                    minNum = arr[j];
+                    check = true;
+                }
+            }
+            
+            answer[i] = check ? minNum : -1;
+        }
+        
+        return answer;
+    }
+}
