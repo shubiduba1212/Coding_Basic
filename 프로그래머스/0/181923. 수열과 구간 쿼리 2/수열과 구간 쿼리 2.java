47eb1bd1a70dd5d065b454bd312ad63e1fa391c5
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-11-06 23:50:00 +0900

    Update 수열과 구간 쿼리 2.java

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = {};
         answer = new int[queries.length];
+        
         for(int i = 0; i < queries.length; i++){
             int start = queries[i][0];
             int end = queries[i][1];
