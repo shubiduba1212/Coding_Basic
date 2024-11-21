1b0120bf6de1afdf93d51e0e06b82cf14d9218da
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-11-21 22:08:32 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.17 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        answer = new int[arr.length];
+        for(int i = 0; i < queries.length; i++){
+            int start = queries[i][0];
+            int end = queries[i][1];
+            //System.out.println("start : "+start);
+            //System.out.println("end : "+end);
+            
+            for(int j = start; j <= end; j++){
+                arr[j] = arr[j]+1;
+                //System.out.println("arr["+j+"] : "+arr[j]);
+            }            
+            
+        }
+        for(int i = 0; i < arr.length; i++){
+            answer[i] = arr[i];
+        }
+        return answer;
+    }
+}
