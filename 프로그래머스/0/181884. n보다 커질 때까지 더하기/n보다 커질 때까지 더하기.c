442e7b225a3583ab65dc85045b42c0169e356aaf
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-12-26 23:59:32 +0900

    Update n보다 커질 때까지 더하기.c

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.c
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdbool.h>
+//#include <stdlib.h>
 
 // numbers_len은 배열 numbers의 길이입니다.
 int solution(int numbers[], size_t numbers_len, int n) {
