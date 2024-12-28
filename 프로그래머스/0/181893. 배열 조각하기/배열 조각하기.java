43d4302bd26faa1c3cd8b12a54d33c8f27d11979
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-12-28 20:35:36 +0900

    [level 0] Title: 배열 조각하기, Time: 6.00 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        int startIdx = 0;
+        
+        for(int i = 0; i < query.length; i++){
+            if(i % 2 == 0){ // 짝수 인덱스라면
+                int[] tempArr = new int[query[i] + 1];
+                for(int j = 0; j < tempArr.length; j++){
+                    tempArr[j] = arr[j];
+                    // System.out.println(tempArr[j]);
+                }
+                arr = tempArr;
+            }else{ // 홀수 인덱스라면
+                int[] tempArr = new int[arr.length - query[i]];
+                startIdx = query[i];
+                for(int j = 0; j < tempArr.length; j++){
+                    tempArr[j] = arr[j + query[i]];
+                    // System.out.println(tempArr[j]);
+                }
+                arr = tempArr;
+            }
+        }
+        
+        return arr;
+    
+    }
+}
