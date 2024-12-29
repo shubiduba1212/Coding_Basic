3763bd930fc21d3331be555e299a31aee162f430
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-12-29 23:46:49 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 125.93 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] solution(int[] arr, boolean[] flag) {
+        int[] answer = {};
+        int length = 0;
+        for(int i = 0; i < flag.length; i++){
+            if(flag[i] == true){                
+                int[] tempArr = new int[length + (arr[i] * 2)];
+                
+                if(answer.length == 0){
+                    int addedNum = arr[i];
+                    for(int j = 0; j < length + (arr[i] * 2); j++){
+                        tempArr[j] = addedNum; 
+                        System.out.println(tempArr[j]);
+                    }
+                }else{
+                    for(int j = 0; j < length; j++){
+                        tempArr[j] = answer[j]; 
+                        System.out.println(tempArr[j]);
+                    }
+                    for(int j = length; j < length + (arr[i] * 2); j++){
+                        int addedNum = arr[i];
+                        tempArr[j] = addedNum; 
+                        System.out.println(tempArr[j]);
+                    }
+                    
+                }
+                length += (arr[i] * 2);
+                answer = tempArr;
+            }else{
+                length -= arr[i];
+                int[] tempArr = new int[length];
+                for(int j = 0; j < length; j++){
+                    tempArr[j] = answer[j]; 
+                    System.out.println(tempArr[j]);
+                }                
+                answer = tempArr;
+            }
+        }
+        return answer;
+    }
+}
