8904a7f775ce39d935150885c863e902e9b0ad62
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-02 22:12:48 +0900

    Update 배열 조각하기.java

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = {};
         
         for(int i = 0; i < query.length; i++){
-            if(i % 2 == 0){ // 짝수 인덱스라면
+            if(i % 2 == 0){
                 int[] tempArr = new int[query[i] + 1];
                 for(int j = 0; j < tempArr.length; j++){
                     tempArr[j] = arr[j];
