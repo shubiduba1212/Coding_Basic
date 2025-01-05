93aea86fb83e95ec93f66eeb6f18339d94d82da6
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-05 22:26:17 +0900

    [level 0] Title: 문자열 묶기, Time: 7930.20 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] tempArr = new int[strArr.length];        
+        for(int i = 0; i < strArr.length; i++){
+            tempArr[i] = strArr[i].length();  
+            //System.out.println(tempArr[i]);
+        }
+        int temp = 0;
+        
+        for(int i = 0; i < tempArr.length; i++){
+            int start = tempArr[i];
+            int count = 0;
+            for(int j = 0; j < tempArr.length; j++){
+                if(start == tempArr[j]){
+                    count++;
+                    temp = count >= temp ? count : temp;                    
+                }
+            }            
+        }
+        answer = temp;
+        return answer;
+    }
+}
