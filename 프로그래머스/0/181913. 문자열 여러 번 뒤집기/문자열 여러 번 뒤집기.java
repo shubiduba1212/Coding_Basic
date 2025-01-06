679d356b8cda806c3e62492f481b16740d3127f8
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-06 23:27:31 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 16.31 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        // char[] copyArr = my_string.toCharArray();
+        
+        for(int i = 0; i < queries.length; i++){
+            int s = queries[i][0];// 2, 0, 5, 6
+            int e = queries[i][1];// 3, 7, 9, 10  
+            char[] tempArr = new char[arr.length];
+            
+            for(int j = 0; j < arr.length; j++){  
+                tempArr[j] = arr[j];
+            }
+            // System.out.println("tempArr : "+ String.valueOf(tempArr));
+            
+            for(int j = 0; j < e - s + 1; j++){  
+                // System.out.println("tempArr["+ (e - j) +"] : "+tempArr[e - j]);
+                // System.out.println("tempArr : "+ String.valueOf(tempArr));
+                arr[s + j] = tempArr[e - j];
+                // System.out.println("arr : "+ String.valueOf(arr));
+            }
+            
+            // System.out.println("after - arr : "+ String.valueOf(arr));
+        }
+    
+        return answer = String.valueOf(arr);
+    }
+}
