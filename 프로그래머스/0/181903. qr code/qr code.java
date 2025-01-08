e51b870e7aec3b3115a7d78a48f2fd6366643eac
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-08 18:00:25 +0900

    [level 0] Title: qr code, Time: 0.17 ms, Memory: 99.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        char[] arr = code.toCharArray();
+        char[] answerArr = {};
+        int count = 0;
+        int idx = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i % q == r){                
+                count++;
+                // System.out.println(i);
+                // System.out.println(arr[i]);
+            }
+        }
+        
+        answerArr = new char[count];
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i % q == r){
+                answerArr[idx] = arr[i];
+                idx++;
+            }
+        }
+        
+        return answer = String.valueOf(answerArr);
+    }
+}
