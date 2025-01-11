398325f73aed3534a3bc71e9d826be572845cc77
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-11 22:54:32 +0900

    [level 0] Title: 2의 영역, Time: 3.91 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int startIdx = 100001;
+        int endIdx = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 2 && startIdx == 100001){
+                startIdx = i;
+                // System.out.println("startIdx : " + i);
+            }else if(arr[i] == 2 && startIdx != 100001){
+                endIdx = i;
+                // System.out.println("endIdx : " + i);
+            }
+        }
+        
+        if(startIdx == 100001){
+            answer = new int[1];
+            answer[0] = -1;
+        }else if(startIdx != 100001 && endIdx == 0){
+            answer = new int[1];
+            answer[0] = 2;
+        }else{
+            answer = new int[(endIdx + 1) - startIdx];
+            int idx = 0;
+            for(int i = startIdx; i < endIdx + 1; i++){
+                answer[idx] = arr[i];
+                idx++;
+                // System.out.println(arr[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
