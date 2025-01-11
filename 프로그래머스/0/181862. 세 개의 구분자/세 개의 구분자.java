8bbcdc30b70a4e38ca5190908170325a84f07d58
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-11 23:33:04 +0900

    [level 0] Title: 세 개의 구분자, Time: 56.07 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String[] solution(String myStr) {
+        String[] answer = {};
+        char[] arr = myStr.toCharArray();
+        int count = 0;        
+                
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 'a' || arr[i] == 'b' || arr[i] == 'c'){
+                arr[i] = 'A';
+            }else{
+                count = 1;
+            }
+            //System.out.println(String.valueOf(arr));
+        }
+        
+        if(count == 0){
+            answer = new String[1];
+            answer[0] = "EMPTY";
+        }else{
+            int empty = 0;
+            int idx = 0;
+            String[] tempArr = String.valueOf(arr).split("A");
+            for(int i = 0; i < tempArr.length; i++){
+                if(tempArr[i].isEmpty()){
+                    empty++;
+                }                 
+            }
+            //System.out.println(empty);
+            answer = new String[tempArr.length - empty];
+            
+            for(int i = 0; i < tempArr.length; i++){
+                if(!tempArr[i].isEmpty()){
+                    answer[idx] = tempArr[i];
+                    idx++;
+                }
+            }
+        }
+        return answer;
+    }
+}
