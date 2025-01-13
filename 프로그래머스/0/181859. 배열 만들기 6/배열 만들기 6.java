6bf8898830d03711a0e7ef54e13efa62e977afd6
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-14 01:43:04 +0900

    [level 0] Title: 배열 만들기 6, Time: 627.70 ms, Memory: 411 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int i = 0;
+        int[] stk = {};
+        
+        while(i < arr.length){
+            //System.out.println(i);
+            if(stk.length == 0){
+                int[] tempArr = new int[1];
+                tempArr[0] = arr[i];
+                stk = tempArr;
+                //System.out.println("stk[0] : " + stk[0]);
+            }else{
+                //System.out.println("stk.length : " + stk.length);
+                if(stk[stk.length - 1] == arr[i]){
+                    int[] tempArr = new int[stk.length - 1];
+                    for(int j = 0; j < tempArr.length; j++){
+                        tempArr[j] = stk[j];
+                        //System.out.println(tempArr[j]);
+                    }
+                    stk = tempArr;  
+                }else{
+                    int[] tempArr = new int[stk.length + 1];
+                    for(int j = 0; j < tempArr.length; j++){
+                        if(j == tempArr.length - 1){
+                            tempArr[j] = arr[i];
+                        }else{
+                            tempArr[j] = stk[j];
+                        }
+                        
+                        //System.out.println(tempArr[j]);
+                    }
+                    stk = tempArr;
+                }
+            }
+            i++;
+        }
+        
+        if(stk.length == 0){
+            int[] tempArr = new int[1];
+            tempArr[0] = -1;
+            stk = tempArr;
+        }
+        return answer = stk;
+    }
+}
