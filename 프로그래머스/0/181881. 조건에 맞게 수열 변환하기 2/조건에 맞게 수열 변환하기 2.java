baded29511d2f6984ee3bd4afd24de70e2d6f537
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-13 23:06:24 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 17.99 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        int[] tempArr = new int[arr.length];
+        int count = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            int temp = 0;
+            while(true){
+                // (arr[i] > 50 && arr[i] % 2 == 1) || (arr[i] < 50 && arr[i] % 2 == 0)
+                if(arr[i] >= 50 && arr[i] % 2 == 0){ // 50보다 크거나 같은 짝수
+                    arr[i] = arr[i] / 2;
+                    // System.out.println("arr["+i+"] : "+ arr[i]);
+                    temp++;
+                }else if(arr[i] < 50 && arr[i] % 2 == 1){ // 50보다 작은 홀수
+                    arr[i] = (arr[i] * 2) + 1;
+                    // System.out.println("arr["+i+"] : "+ arr[i]);
+                    temp++;
+                }else{
+                    break;
+                }
+            } 
+            count = temp >= count ? temp : count;
+            // System.out.println("count : "+ count);
+        }
+        
+        return answer = count;
+    }
+}
