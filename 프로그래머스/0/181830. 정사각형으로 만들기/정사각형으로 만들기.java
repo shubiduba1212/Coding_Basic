1376d27447740acbdaeb6e8a3d069196a22b3081
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-14 21:09:26 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.54 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int[][] answer = {};
+                
+        if(arr[0].length > arr.length){
+            int length = arr[0].length;
+            int[][] tempArr = new int[length][length];
+            for(int i = 0; i < tempArr.length; i++){                    
+                for(int j = 0; j < tempArr.length; j++){
+                    if(i >= arr.length){
+                        tempArr[i][j] = 0;
+                    }else{
+                        tempArr[i][j] = arr[i][j];
+                    }
+                }                
+            }
+            answer = tempArr;
+            //System.out.println(arr[i].length);
+            
+        }else if(arr[0].length < arr.length){
+            int length = arr.length;
+            int[][] tempArr = new int[length][length];
+            for(int i = 0; i < tempArr.length; i++){
+                for(int j = 0; j < tempArr.length; j++){
+                    if(j >= arr[i].length){
+                        tempArr[i][j] = 0;
+                    }else{
+                        tempArr[i][j] = arr[i][j];
+                    }
+                }
+            }
+            answer = tempArr;
+        }else{
+            answer = arr;
+        }
+        
+        return answer;
+    }
+}
