3e185cf6c5448fd0e3aca3d86f32ade901216c38
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-01-14 23:22:42 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.70 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;  
+        int idx = 0;
+        int[][] trueList = {};
+        
+        for(int i = 0; i < rank.length; i++){            
+            if(attendance[i] == true){
+                int[][] tempArr = new int[idx + 1][2];
+                
+                // System.out.println("tempArr["+idx+"]["+0+"] : " + tempArr[idx][0]);
+                // System.out.println("tempArr["+idx+"]["+1+"] : " + tempArr[idx][1]);
+                for(int j = 0; j < tempArr.length; j++){
+                    if(j >= idx){
+                        tempArr[idx][0] = i;
+                        tempArr[idx][1] = rank[i];
+                    }else{
+                        tempArr[j][0] = trueList[j][0];
+                        tempArr[j][1] = trueList[j][1];
+                    }
+                }
+                idx++;
+                trueList = tempArr;
+            }
+        }
+        
+        for(int i = 0; i < trueList.length - 1; i++){                        
+            for(int j = 0; j < trueList.length - 1 - i; j++){                                
+                if(trueList[j][1] > trueList[j+1][1]){
+                    int temp1 = trueList[j][0];
+                    int temp2 = trueList[j][1];
+                    
+                    trueList[j][0] = trueList[j+1][0];
+                    trueList[j][1] = trueList[j+1][1];
+                    trueList[j+1][0] = temp1;
+                    trueList[j+1][1] = temp2;
+                    // System.out.println("trueList["+j+"]["+0+"] : " + trueList[j][0]);
+                    // System.out.println("trueList["+j+"]["+1+"] : " + trueList[j][1]);
+                }
+            }
+        }
+        
+        return answer = (trueList[0][0] * 10000) + (trueList[1][0] * 100) + trueList[2][0];
+    }
+}
