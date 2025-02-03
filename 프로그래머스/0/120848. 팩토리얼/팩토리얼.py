6c5d31b9f685f815393b83d9efe40b70b626346a
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-03 21:05:43 +0900

    Update 팩토리얼.py

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -2,13 +2,9 @@ def solution(n):
     answer = 1
     facNum = 1
     
-    while facNum < n :        
-        if facNum >= n :
-            print(answer)
-            break  
-        else :
-            answer += 1    
-            facNum *= answer   
+    while facNum < n :    
+        answer += 1    
+        facNum *= answer   
     
     if facNum != n :
         answer -= 1
