f86e992357286786b8110b6f0be6d67d4939e872
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-09 00:19:54 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -0,0 +1,26 @@
+def solution(lines):
+    answer = 0
+    position = []
+
+    for i in range(len(lines) - 1):
+        for j in range(i + 1, len(lines)):
+            first = max(lines[i][0], lines[j][0])
+            second = min(lines[i][1], lines[j][1])
+
+            if first < second:  
+                position.append([first, second])
+
+    
+    position.sort()
+    merged_intervals = []
+    
+    for start, end in position:
+        if merged_intervals and merged_intervals[-1][1] >= start:  
+            
+            merged_intervals[-1][1] = max(merged_intervals[-1][1], end)
+        else:
+            merged_intervals.append([start, end])
+
+    answer = sum(end - start for start, end in merged_intervals)
+
+    return answer
