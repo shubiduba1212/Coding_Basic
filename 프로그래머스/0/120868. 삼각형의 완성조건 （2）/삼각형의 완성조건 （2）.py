c4e1443ee9b0663327a49bb501afd73ae2cda698
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-21 23:50:36 +0900

    Update 삼각형의 완성조건 （2）.py

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -6,11 +6,9 @@ def solution(sides):
         answer = 1
     else :
         for i in range((sides[1] + 1) - sides[0] ,(sides[1] + 1)) :
-            # print(f"가장 긴 변 : {sides[1]} 일때 : {i}")
             answer += 1
 
         for i in range(sides[1] + 1, (sides[0] + sides[1])) :
-            # print(f"가장 긴 변 : {sides[0] + sides[1]} 보다 작을때 : {i}")
             answer += 1
             
     return answer
