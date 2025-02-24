3cf0908ace3dcb59ac9374859401c3b1271926af
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-24 23:04:23 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -5,7 +5,6 @@ def solution(arr):
         if arr[i] != temp :
             answer.append(arr[i])
             temp = arr[i]
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     # print('Hello Python')
     # for i in arr :        
     #     if not answer or answer[-1] != i :
