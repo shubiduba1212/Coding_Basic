2e542e89db68ebf3a1e40522aec6a2e2431519b0
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-24 23:01:07 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,21 @@
+from collections import deque
+
 def solution(progresses, speeds):
     answer = []
-    temp = []
-    for i in range(len(progresses)) :
-        rest = ((100 - progresses[i]) // speeds[i]) + 1 if (100 - progresses[i]) % speeds[i] > 0 else (100 - progresses[i]) // speeds[i]
-        temp.append(rest)
+    queue = deque()
     
-    length = len(temp)
-    count = 0
-    date = temp[0]
-    for i in range(length) :        
-        # 앞 기능의 작업 완료일보다 짧거나 같은 경우
-        if temp[i] <= date :
-            count += 1
-        else : 
-            answer.append(count)
-            count = 0
-            count += 1
-            date = temp[i]
-            print(date)
+    for p, s in zip(progresses, speeds):
+        days = (100 - p) // s + (1 if (100 - p) % s > 0 else 0)
+        queue.append(days)
+    
+    while queue :
+        day = queue.popleft()
+        count = 1
         
-        if i == length - 1 :
-            answer.append(count)
+        while queue and queue[0] <= day :
+            queue.popleft()
+            count += 1
+        
+        answer.append(count)
     
     return answer
