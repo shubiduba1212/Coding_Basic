e54c48a3bef1bd5c9eaf2ee0629e6e125376e021
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-02-25 22:17:51 +0900

    [level 2] Title: 올바른 괄호, Time: 6.54 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = []
+    
+    for i in s :
+        if i == '(' :
+            stack.append(i)
+        else :
+            if not stack :
+                return False
+            stack.pop()
+     
+    return len(stack) == 0
