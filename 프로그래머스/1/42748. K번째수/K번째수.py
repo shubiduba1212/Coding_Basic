0c2a6632fb525702c5694aabb1d97f86501cfc41
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-05-04 23:51:57 +0900

    Update K번째수.py

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,7 +2,6 @@ def solution(array, commands):
     answer = []
     
     for i in commands :
-        # print(array[(i[0] - 1) : (i[1])])
         temp = array[(i[0] - 1) : (i[1])]
         temp.sort()
         answer.append(temp[(i[2] - 1)])
