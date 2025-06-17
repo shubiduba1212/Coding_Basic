65f9a19a31a3459d39b24f65b90fe0117648f82f
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-06-17 13:43:37 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = [];
+    answer = my_string.split('')
+                      .filter(char => !Number.isNaN(Number(char)))
+                      .map(Number)
+                      .sort((a,b) => a - b);
+    return answer;
+}
