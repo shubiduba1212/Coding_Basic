9d5d5b7608bd87f8303bedced9d639dc3e5f452d
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-06-18 23:58:38 +0900

    Update 문자열 정렬하기 （1）.js

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,8 +1,15 @@
 function solution(my_string) {
     var answer = [];
-    answer = my_string.split('')
-                      .filter(char => !Number.isNaN(Number(char)))
-                      .map(Number)
-                      .sort((a,b) => a - b);
+    // answer = my_string.split('')
+    //                   .filter(char => !Number.isNaN(Number(char)))
+    //                   .map(Number)
+    //                   .sort((a,b) => a - b);
+    for (let char of my_string) {
+        const num = Number(char);
+        if (!Number.isNaN(num)) {
+            answer.push(num);
+        }
+    }
+    answer.sort((a, b) => a - b);
     return answer;
 }
