63d0978c8410262a7b57ce11cd6d83404389100b
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-06-29 17:41:15 +0900

    Update 배열 회전시키기.js

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,20 @@
 function solution(numbers, direction) {
+    var answer = [];
     if(direction == 'right'){
-        const lastNum = numbers.pop();
-        numbers.unshift(lastNum);
+        answer = [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
     }else{
-        const firstNum = numbers.shift();
-        numbers.push(firstNum);
+        answer = [...numbers.slice(1), numbers[0]]
     }
-    return numbers;
+    return answer;
+    
+    // if(direction == 'right'){
+    //     const lastNum = numbers.pop();
+    //     numbers.unshift(lastNum);
+    // }else{
+    //     const firstNum = numbers.shift();
+    //     numbers.push(firstNum);
+    // }
+    // return numbers;
+    // shift()/unshift()처럼 원본 배열의 요소를 물리적으로 "밀거나 당기는" 방식 
+    // 정확히는 shift/unshift의 구현 방식에서 발생하는 비용이 더 큽니다.
 }
