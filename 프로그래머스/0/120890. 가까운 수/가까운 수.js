d1af5061ea88aa45de657d28f9c292424c8eba98
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2025-07-17 22:42:17 +0900

    Update 가까운 수.js

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -2,10 +2,5 @@ function solution(array, n) {
     var answer = 0;
     const minNum = Math.min.apply(null, array.filter(item => item - n >= 0));
     const maxNum = Math.max.apply(null, array.filter(item => item - n <= 0));
-    // if((minNum - n) >= Math.abs(n - maxNum)){
-    //     answer = maxNum;
-    // }else{
-    //     answer
-    // }
     return answer = (minNum - n) >= Math.abs(n - maxNum) ? maxNum : minNum;
 }
